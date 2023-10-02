56bda0521f07fa410b64e39bca3ca0c3c3e8c7dd
aletxapascual <aletxa.pasacual@gmail.com>
2023-10-02 16:35:00 -0600

    .

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Route, Routes, useActionData } from "react-router-dom";
+import './App.css';
+import Inventory from './pages/Inventory.js'
+import Login from './pages/Login.js'
+import PrintableSheet from "./pages/PrintableSheet";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/inventory" element = {<Inventory />}/>
+          <Route path="/login" element = {<Login />}/>
+          <Route path="/printableSheet" element = {<PrintableSheet />}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from 'react'
+
+function Login() {
+  const [number, setNumber] = useState(8733);
+  const [data, setData]=useState([]);
+
+  function getCodes(){
+    fetch('https://script.google.com/macros/s/AKfycbw9W1Ctu9286GWSJf6Db2HyNY7gKzbjVLE3QvAompXgf8Gk3K902DiUIvH9nWSx9_v0zQ/exec')
+    .then(response=>response.json())
+    .then(response=>setData(response))
+  }
+  useEffect(()=>{ 
+    getCodes()
+  },[])
+
+  function clickLogin(){
+    console.log(number)
+    let matchingRow = null;
+    for(let i = 0; i < data.length; i ++){ 
+      if(number==data[i][0]){
+        matchingRow = data[i];
+        window.location.href = '/PrintableSheet?code='+number+'#'+matchingRow;
+      }
+    }
+  }
+    
+    return (
+    <div>
+      <h1>Login</h1>
+      <div>
+        <input type="number" value={number} onChange={(e)=>setNumber(e.target.value)}/>
+
+      </div>
+      <button onClick={clickLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login
